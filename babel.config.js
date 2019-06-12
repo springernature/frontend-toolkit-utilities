ffdbc90be4d82a1966372cd66fda408f6a67423a
jpw <jpw@users.noreply.github.com>
2019-06-12 09:52:27 +0100

    reconfigures babel, hikes deps (#11)

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+/*
+	A note on this file & monorepos
+	https://babeljs.io/docs/en/config-files#monorepos
+*/
+
+module.exports = function (api) {
+	api.cache(true);
+
+	const presets = ['@babel/preset-env'];
+	const plugins = [];
+
+	return {
+		presets,
+		plugins
+	};
+};
