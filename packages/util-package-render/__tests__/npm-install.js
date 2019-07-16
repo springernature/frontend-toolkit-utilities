1e9aef4581c1cb23c8cb07578c809a79c5f3adc2
jpw <jon@mdi.eu.com>
2019-07-16 15:29:12 +0100

    refactor and better spying

--- packages/util-package-render/__tests__/npm-install.js
+++ packages/util-package-render/__tests__/npm-install.js
@@ -5,58 +5,71 @@ const install = require('../utils/npm-install');
 const mockDependencies = require('../__mocks__/data/npm-dependencies');
 
 describe('Utility: npm-install', () => {
+
+	let dependenciesSpy;
+	beforeEach(() => {
+		dependenciesSpy = jest.spyOn(install, 'dependencies');
+	});
+
+	afterEach(() => {
+		dependenciesSpy.mockRestore();
+	});
+
+	describe('dependencies', () => {
+		test('calls getValidDepdendencies', async () => {
+			expect.assertions(1);
+			await install.dependencies();
+			expect(dependenciesSpy).toHaveBeenCalledTimes(1);
+		});
+	});
+
 	describe('devDependencies', () => {
 		test('calls dependencies', async () => {
 			expect.assertions(1);
-			install.dependencies = jest.fn();
-			install.devDependencies();
-			expect(install.dependencies).toHaveBeenCalledTimes(1);
+			await install.devDependencies();
+			expect(dependenciesSpy).toHaveBeenCalledTimes(1);
 		});
 	});
 
 	describe('peerDependencies', () => {
 		test('calls dependencies', async () => {
 			expect.assertions(1);
-			install.dependencies = jest.fn();
-			install.peerDependencies();
-			expect(install.dependencies).toHaveBeenCalledTimes(1);
+			await install.peerDependencies();
+			expect(dependenciesSpy).toHaveBeenCalledTimes(1);
 		});
 	});
 
 	describe('getValidDepdendencies', () => {
-		test('npmExampleVersionRanges are valid', async () => {
+		test('npmExampleVersionRanges are valid', () => {
 			expect.assertions(1);
 			expect(
 				install.getValidDepdendencies(mockDependencies.npmExampleDependencies)
-			)
-			.toStrictEqual(mockDependencies.npmExampleDependenciesAsArray);
+				)
+				.toStrictEqual(mockDependencies.npmExampleDependenciesAsArray);
 		});
 
-		test('does not return bad names', async () => {
+		test('does not return bad names', () => {
 			expect.assertions(1);
 			expect(
 				install.getValidDepdendencies(mockDependencies.badNames)
-			)
-			.toStrictEqual([]);
+				)
+				.toStrictEqual([]);
 		});
 
-		test('does not return bad values', async () => {
+		test('does not return bad values', () => {
+			expect.assertions(1);
+				expect(
+					install.getValidDepdendencies(mockDependencies.badValues)
+				)
+				.toStrictEqual([]);
+			});
+
+		test('does not return old-style npm package names', () => {
 			expect.assertions(1);
 			expect(
-				install.getValidDepdendencies(mockDependencies.badValues)
+					install.getValidDepdendencies(mockDependencies.oldStyleNPMDependencies)
 			)
 			.toStrictEqual([]);
 		});
-
-		test('does not return old-style npm package names', async () => {
-			expect.assertions(1);
-			expect(
-				install.getValidDepdendencies(mockDependencies.oldStyleNPMDependencies)
-			)
-			.toStrictEqual([]);
-		});
-
 	});
-
-
 });
