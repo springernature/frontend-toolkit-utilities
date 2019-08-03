43f7f5843ac05bda16527c3aca9a91e709d86a68
Alex Kilgour <alexjkilgour@gmail.com>
2019-08-03 16:34:58 +0100

    tidy up how configuration is passed

--- packages/util-package-extender/lib/utils/_get-remote-file.js
+++ packages/util-package-extender/lib/utils/_get-remote-file.js
@@ -9,10 +9,35 @@
 const got = require('got');
 
 // Number of times to retry request
-// Retry on the following:
-// statusCodes: 408 413 429 500 502 503 504
-// errorCodes: ETIMEDOUT ECONNRESET EADDRINUSE ECONNREFUSED EPIPE ENOTFOUND ENETUNREACH EAI_AGAIN
 const maxRetry = 2;
+
+// Request configuration options
+const config = {
+	retry: {
+		retries: maxRetry,
+		statusCodes: new Set([404, 408, 413, 429, 500, 502, 503, 504]),
+		errorCodes:
+			new Set([
+				'ETIMEDOUT',
+				'ECONNRESET',
+				'EADDRINUSE',
+				'ECONNREFUSED',
+				'EPIPE',
+				'ENOTFOUND',
+				'ENETUNREACH',
+				'EAI_AGAIN'
+			])
+	},
+	hooks: {
+		beforeRetry: [
+			(options, error, retryCount) => {
+				console.log(`fail: requesting ${options.href}`);
+				console.log(`info: ${generateErrorMessage(error)}`);
+				console.log(`info: retrying (${retryCount}/${maxRetry})`);
+			}
+		]
+	}
+};
 
 /**
  * Generate an error message based on error type
@@ -42,18 +67,7 @@ function getRemoteFile(url) {
 	return new Promise((resolve, reject) => {
 		(async () => {
 			try {
-				const response = await got(url, {
-					retry: maxRetry,
-					hooks: {
-						beforeRetry: [
-							(_options, error, retryCount) => {
-								console.log(`fail: requesting ${url}`);
-								console.log(`info: ${generateErrorMessage(error)}`);
-								console.log(`info: retrying (${retryCount}/${maxRetry})`);
-							}
-						]
-					}
-				});
+				const response = await got(url, config);
 				resolve(response.body);
 			} catch (error) {
 				console.log(`fail: requesting ${url}`);
