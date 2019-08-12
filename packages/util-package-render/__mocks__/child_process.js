47917545a10242858633d5af0cad4e04f25170c1
jpw <jon@mdi.eu.com>
2019-08-12 11:35:54 +0100

    rms duplicated data in child_process.js

--- packages/util-package-render/__mocks__/child_process.js
+++ packages/util-package-render/__mocks__/child_process.js
@@ -1,11 +1,14 @@
 'use strict';
-
+const mockDependencies = require('data/npm-dependencies');
 const childProcess = jest.genMockFromModule('child_process');
 
 // in node, returns a <ChildProcess>
 
+const knownToThrowEntry = Object.entries(mockDependencies.oneKnownToThrowDependency)[0];
+const knownToThrowPackage = knownToThrowEntry[0] + '@' + knownToThrowEntry[1];
+
 childProcess.exec = jest.fn((command, cb) => {
-	if (command === 'npm install ohno@666') {
+	if (command === `npm install ${knownToThrowPackage}`) {
 		// known-bad package which should throw
 		cb(
 			'An error message from child_process mock',
