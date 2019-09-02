d188a8112f372aa4c777236fa7401978c683f271
Alex Kilgour <alexjkilgour@gmail.com>
2019-09-02 15:59:01 +0100

    util-package-extender - try and fix weird travis bug

--- packages/util-package-extender/__tests__/unit/_utils/merge-packages.js
+++ packages/util-package-extender/__tests__/unit/_utils/merge-packages.js
@@ -12,7 +12,48 @@ jest.mock('../../../lib/js/_utils/_get-remote-file');
 const mergePackages = require('../../../lib/js/_utils/_merge-packages');
 const results = require('../../../__mocks__/filesystem-results');
 
-describe('Merge Two packages together', () => {
+describe('Fail to merge Two packages together', () => {
+	beforeEach(() => {
+		mockfs(MOCK_PACKAGES);
+		jest.resetModules();
+
+	});
+
+	test('Reject when remote package doesn\'t exist', async () => {
+		expect.assertions(1);
+		await expect(
+			mergePackages(
+				{
+					local: results.local,
+					remote: results.merged
+				},
+				'path/to/local/current',
+				'fake-remote-package@2.0.0'
+			)
+		).rejects.toBeInstanceOf(Error);
+	});
+
+	test('Reject when a local file does not exist', async () => {
+		expect.assertions(1);
+		await expect(
+			mergePackages(
+				{
+					local: [...results.local, 'file-not-found.ext'],
+					remote: results.merged
+				},
+				'path/to/local/current',
+				'remote-package@1.0.0',
+				'path/to/local/current/subdir'
+			)
+		).rejects.toBeInstanceOf(Error);
+	});
+
+	afterEach(() => {
+		mockfs.restore();
+	});
+});
+
+describe('Successfully merge Two packages together', () => {
 	beforeEach(() => {
 		mockfs(MOCK_PACKAGES);
 		jest.resetModules();
@@ -47,35 +88,6 @@ describe('Merge Two packages together', () => {
 			)
 		).resolves.toEqual();
 	});
-
-	test('Reject when remote package doesn\'t exist', async () => {
-		expect.assertions(1);
-		await expect(
-			mergePackages(
-				{
-					local: results.local,
-					remote: results.merged
-				},
-				'path/to/local/current',
-				'fake-remote-package@2.0.0'
-			)
-		).rejects.toBeInstanceOf(Error);
-	});
-
-	// test('Reject when a local file doesn\'t exist', async () => {
-	// 	expect.assertions(1);
-	// 	await expect(
-	// 		mergePackages(
-	// 			{
-	// 				local: [...results.local, 'file-not-found.ext'],
-	// 				remote: results.merged
-	// 			},
-	// 			'path/to/local/current',
-	// 			'remote-package@1.0.0',
-	// 			'path/to/local/current/subdir'
-	// 		)
-	// 	).rejects.toBeInstanceOf(Error);
-	// });
 
 	afterEach(() => {
 		mockfs.restore();
