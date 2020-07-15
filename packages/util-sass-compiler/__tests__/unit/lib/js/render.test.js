5639c42645ccdacd1c141bf83e54536aa0836023
Alex Kilgour <alexjkilgour@gmail.com>
2020-07-15 15:55:24 +0100

    add sass compiler modue

---
+++ packages/util-sass-compiler/__tests__/unit/lib/js/render.test.js
@@ -0,0 +1,143 @@
+/**
+ * __tests__/unit/lib/js/render.test.js
+ * Test: lib/js/render.js
+ */
+'use strict';
+
+const path = require('path');
+const sass = require('node-sass');
+import {render} from '../../../../lib/js/render';
+
+const importsLocation = path.resolve(__dirname, '../../../../scss/imports');
+
+// Data
+
+const cssData = `
+	.foo {
+		width: 100px;
+	}`;
+
+const nestedSassData = `
+	.foo {
+		width: 100px;
+		&:last {
+			width: 200px;
+		}
+	}`;
+
+const functionErrData = `
+	@import '${importsLocation}';
+
+	.foo {
+		width: half('string');
+	}`;
+
+const functionWarnData = `
+	@import '${importsLocation}';
+
+	.foo {
+		width: half(9px);
+	}`;
+
+// Tests
+
+describe('Compile SASS', () => {
+	test('plain CSS from data', async () => {
+		const result = await render({data: cssData});
+		expect(result.css.toString().trim()).toEqual('.foo{width:100px}');
+	});
+
+	test('nested SASS from data', async () => {
+		const result = await render({data: nestedSassData});
+		expect(result.css.toString().trim()).toEqual('.foo{width:100px}.foo:last{width:200px}');
+	});
+
+	test('plain CSS from file', async () => {
+		const result = await render({
+			file: path.resolve(__dirname, '../../../../scss/css.css')
+		});
+		expect(result.css.toString().trim()).toEqual('.foo{width:100px}');
+	});
+
+	test('nested SASS from file', async () => {
+		const result = await render({
+			file: path.resolve(__dirname, '../../../../scss/nested.scss')
+		});
+		expect(result.css.toString().trim()).toEqual('.foo{width:100px}.foo:last{width:200px}');
+	});
+
+	test('imports don\'t output CSS', async () => {
+		const result = await render({
+			file: path.resolve(__dirname, '../../../../scss/_imports.scss')
+		});
+		expect(result.css.toString().trim()).toEqual('');
+	});
+
+	test('SASS function', async () => {
+		const result = await render({
+			file: path.resolve(__dirname, '../../../../scss/function.scss')
+		});
+		expect(result.css.toString().trim()).toEqual('.foo{width:5px}');
+	});
+
+	test('SASS function error', async () => {
+		return expect(render({data: functionErrData})).rejects.toThrow(
+			'not a number'
+		)
+	});
+
+	test('SASS function warning', async () => {
+		const mockWarnFunction = jest.fn()
+			.mockReturnValue(sass.NULL)
+
+		return render({
+			data: functionWarnData,
+			functions: {
+				'@warn': mockWarnFunction
+			}
+		}).then(() => {
+			return expect(mockWarnFunction.mock.calls[0][0].getValue())
+				.toEqual('non-whole number 9px')
+		})
+	});
+
+	test('SASS mixin', async () => {
+		const result = await render({
+			file: path.resolve(__dirname, '../../../../scss/mixin.scss')
+		});
+		expect(result.css.toString().trim()).toEqual('.foo{background-color:blue;border:1px solid red;padding:10px}');
+	});
+
+	test('no data or file provided', async () => {
+		return expect(render()).rejects.toThrow(
+			'No input specified: provide a file name or a source string to process'
+		)
+	});
+
+	// Check returned data as JSON
+
+	test('JSON class match', async () => {
+		const result = await render({data: cssData});
+		expect(result.json).toEqual(
+			expect.objectContaining({
+				'.foo': {
+					'width': '100px'
+				}
+			})
+		);
+	});
+
+	test('JSON full object', async () => {
+		const result = await render({data: nestedSassData});
+		expect(result.json).toEqual(
+			{
+				'.foo': {
+					'width': '100px'
+				},
+				'.foo:last': {
+					'width': '200px'
+				}
+			}
+		);
+	});
+});
---
+++ packages/util-sass-compiler/lib/js/render.js
@@ -0,0 +1,43 @@
+/**
+ * render.js
+ * Promisify SASS.render and add JSON output
+ */
+'use strict';
+
+const util = require('util');
+
+const css2json = require('css2json');
+const sass = require('node-sass');
+
+const sassRender = util.promisify(sass.render);
+
+/**
+ * Generate a compiled SASS object
+ * @param {Object} options node sass options
+ * @return {Object}
+ */
+module.exports.render = async options => {
+	try {
+		const sassObject = await sassRender({
+			// Where node-sass should look for files when you use @import
+			includePaths: [__dirname],
+			// Using a compact/compressed output style allows you to use concise 'expected' CSS
+			outputStyle: 'compressed',
+			// Merge in any other options you pass when calling render
+			// Should include `file` or `data`
+			...options
+		});
+
+		// Return SASS object including JSON variant
+		return {
+			...sassObject,
+			...{
+				json: css2json(
+					sassObject.css.toString()
+				)
+			}
+		};
+	} catch (error) {
+		throw new Error(error.message);
+	}
+};
