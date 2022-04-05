d5400d01c86f8a5ed14d04802c1454f6f355ce69
Alex Kilgour <alexjkilgour@gmail.com>
2022-04-05 18:39:46 +0100

    change the way options are passed and allow turning reporting off

--- packages/util-package-installer/lib/js/installer.js
+++ packages/util-package-installer/lib/js/installer.js
@@ -15,6 +15,35 @@ const validatePackageName = require('validate-npm-package-name');
 const VERSION_RANGE_MAXLENGTH = 50;
 
 /**
+ * Validate options for installing package
+ * @private
+ * @param  {Object} options installation options
+ * @returns {Boolean} are the options valid
+ */
+function validateOptionsObject(options) {
+	const validOptions = ['arguments', 'reporting', 'prefix'];
+	const keysAreValid = Object.keys(options).every(item => validOptions.includes(item));
+
+	if (!keysAreValid) {
+		return false;
+	}
+
+	if (options.arguments && !Array.isArray(options.arguments)) {
+		return false;
+	}
+
+	if (options.reporting && typeof options.reporting !== 'boolean') {
+		return false;
+	}
+
+	if (options.prefix && typeof options.prefix !== 'string') {
+		return false;
+	}
+
+	return true;
+}
+
+/**
  * A Object representing a JSON.parse-d package.json.
  * @typedef {Object.<string, string>} PackageJSON
  */
@@ -27,31 +56,41 @@ module.exports = {
 	/**
 	 * Main method which actually installs given depdendencies.
 	 * @param  {Dependencies} dependencies={}
-	 * @param  {String} options='' any parameters to pass to npm install e.g. --no-save
-	 * @param  {String} prefix path to custom install location
+	 * @param  {Object} options={} user settings for installation
 	 * @returns {Promise} resolves with "npm install" command stdout data.
 	 * @throws {Error} message is contents of "npm install" command stderr, or some other operational error.
 	 */
-	dependenciesObject: async (dependencies = {}, options = '', prefix) => {
+	dependenciesObject: async (dependencies = {}, options = {}) => {
 		const validDepdendencies = module.exports.getValidDepdendencies(dependencies);
 		const packageList = validDepdendencies.map(dep => dep.join('@'));
 		const packageListAsStr = packageList.join(' ');
-		const optionsAsArr = (options && options.length > 0) ? options.trim().split(' ') : [];
 		const prefixInstallCommand = [];
 
-		// Generate prefix install command
-		if (prefix) {
-			prefixInstallCommand.push('--prefix', prefix);
-		}
+		// Merge options with defaults
+		options = Object.assign({reporting: true, arguments: []}, options);
 
-		reporter.info('npm install', packageListAsStr);
-
+		// Validate dependencies
 		if (!packageListAsStr || packageListAsStr === '') {
 			throw new Error('invalid package list');
 		}
 
+		// Validate options
+		if (!validateOptionsObject(options)) {
+			throw new Error('Invalid options. Valid options - arguments:array, reporting:boolean, prefix:string');
+		}
+
+		// Generate prefix install command
+		if (options.prefix) {
+			prefixInstallCommand.push('--prefix', options.prefix);
+		}
+
+		// Report info to CLI
+		if (options.reporting) {
+			reporter.info('npm install', packageListAsStr);
+		}
+
 		const spawnPromiseResolution = await new Promise((resolve, reject) => {
-			const installCommand = prefixInstallCommand.concat(['install'], optionsAsArr);
+			const installCommand = prefixInstallCommand.concat(['install'], options.arguments);
 			const child = cp.spawn('npm', installCommand.concat(packageList));
 
 			let childStdout = '';
@@ -93,34 +132,41 @@ module.exports = {
 	/**
 	 * Helper to install just the dependencies in a parsed package.json
 	 * @param  {PackageJSON} packageJSON={}
-	 * @param  {String} options any parameters to pass to npm install e.g. --no-save
-	 * @param  {String} prefix specify an install location
+	 * @param  {Object} options={}
+	 * @param  {Array} options.arguments any parameters to pass to npm install e.g. --no-save
+	 * @param  {Boolean} options.reporting show CLI reporting
+	 * @param  {String} options.prefix specify an install location
 	 */
-	dependencies: async (packageJSON = {}, options, prefix) =>
-		module.exports.dependenciesObject(packageJSON.dependencies, options, prefix),
+	dependencies: async (packageJSON = {}, options = {}) =>
+		module.exports.dependenciesObject(packageJSON.dependencies, options),
 
 	/**
 	 * Helper to install just the devDependencies in a parsed package.json
 	 * @param  {PackageJSON} packageJSON={}
-	 * @param  {String} options any parameters to pass to npm install e.g. --no-save
-	 * @param  {String} prefix specify an install location
+	 * @param  {Object} options={}
+	 * @param  {Array} options.arguments any parameters to pass to npm install e.g. --no-save
+	 * @param  {Boolean} options.reporting show CLI reporting
+	 * @param  {String} options.prefix specify an install location
 	 */
-	devDependencies: async (packageJSON = {}, options, prefix) =>
+	devDependencies: async (packageJSON = {}, options = {}) =>
 		module.exports.dependenciesObject(
 			packageJSON.devDependencies,
-			(options) ? `${options} --save-dev` : '--save-dev',
-			prefix
+			// Merge --save-dev option into the arguments array
+			Object.assign(options, {
+				arguments: (options.arguments && Array.isArray(options.arguments)) ? options.arguments.concat(['--save-dev']) : ['--save-dev']
+			})
 		),
 
 	/**
 	 * Helper to install just the peerDependencies in a parsed package.json
 	 * @param  {PackageJSON} packageJSON={}
-	 * @param  {String} options any parameters to pass to npm install e.g. --no-save
-	 * @param  {String} prefix specify an install location
-	 * TODO: this will mangle the depedencies
+	 * @param  {Object} options={}
+	 * @param  {Array} options.arguments any parameters to pass to npm install e.g. --no-save
+	 * @param  {Boolean} options.reporting show CLI reporting
+	 * @param  {String} options.prefix specify an install location
 	 */
-	peerDependencies: async (packageJSON = {}, options, prefix) =>
-		module.exports.dependenciesObject(packageJSON.peerDependencies, options, prefix),
+	peerDependencies: async (packageJSON = {}, options = {}) =>
+		module.exports.dependenciesObject(packageJSON.peerDependencies, options),
 
 	/** Maximum permitted length of a package version range string */
 	VERSION_RANGE_MAXLENGTH: VERSION_RANGE_MAXLENGTH
