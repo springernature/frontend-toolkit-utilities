85917e177fc1f5f266bb673ec481d085c31f2097
Alex Kilgour <alexjkilgour@gmail.com>
2022-10-20 14:52:58 +0100

    Update index.test.js

--- packages/util-package-diff/__tests__/unit/lib/index.test.js
+++ packages/util-package-diff/__tests__/unit/lib/index.test.js
@@ -9,7 +9,7 @@ jest.mock('@springernature/util-cli-reporter');
 // const diffPackage = require('../../../lib');
 
 describe('package diff', () => {
-	test('diff two packages', async () => {
+	test('diff two packages', () => {
 		// await diffPackage('global-package@1.0.0', '@springernature', 3000);
 		expect(1 + 1).toBe(2);
 	});
