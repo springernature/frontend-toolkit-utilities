b1c74fb3d3215ccb2ccc22a570fb7a6c541702b3
Alex Kilgour <alexjkilgour@gmail.com>
2022-10-20 13:37:43 +0100

    history, readme, test placeholder

---
+++ packages/util-package-diff/HISTORY.md
@@ -0,0 +1,5 @@
+# History
+
+## 0.0.1 (2022-10-20)
+	* Initial commit
+	* Experimental version, no tests yet
---
+++ packages/util-package-diff/__tests__/unit/lib/index.test.js
@@ -0,0 +1,16 @@
+/**
+ * __tests__/unit/lib/index.test.js
+ * Test: lib/index.js
+ */
+'use strict';
+
+jest.mock('@springernature/util-cli-reporter');
+
+const diffPackage = require('../../../lib');
+
+describe('package diff', () => {
+	test('diff two packages', async () => {
+		// await diffPackage('global-package@1.0.0', '@springernature', 3000);
+		expect(1 + 1).toBe(2);
+	});
+});
